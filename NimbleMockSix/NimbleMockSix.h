9e06667ce2123fe2cc3637d91e02a987d35d9b49
Tamas Lustyik <elveestei@gmail.com>
2017-01-01 22:40:09 +0100

    Initial Commit

---
+++ NimbleMockSix/NimbleMockSix.h
@@ -0,0 +1,19 @@
+//
+//  NimbleMockSix.h
+//  NimbleMockSix
+//
+//  Created by Tamas Lustyik on 2017. 01. 01..
+//  Copyright © 2017. Tamas Lustyik. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NimbleMockSix.
+FOUNDATION_EXPORT double NimbleMockSixVersionNumber;
+
+//! Project version string for NimbleMockSix.
+FOUNDATION_EXPORT const unsigned char NimbleMockSixVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NimbleMockSix/PublicHeader.h>
+
+
