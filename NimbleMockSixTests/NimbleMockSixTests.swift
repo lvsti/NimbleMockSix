3be1d793f53c2861a12ae363a06b48f1b894bb1d
Tamas Lustyik <elveestei@gmail.com>
2017-01-04 20:39:50 +0100

    Created unit test targets

---
+++ NimbleMockSixTests/NimbleMockSixTests.swift
@@ -0,0 +1,16 @@
+//
+//  NimbleMockSixTests.swift
+//  NimbleMockSixTests
+//
+//  Created by Tamas Lustyik on 2017. 01. 04..
+//  Copyright © 2017. Tamas Lustyik. All rights reserved.
+//
+
+import Quick
+import Nimble
+import MockSix
+import NimbleMockSix
+
+class NimbleMockSixSpec: QuickSpec {
+    
+}
